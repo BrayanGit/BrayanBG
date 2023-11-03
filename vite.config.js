bfd5394b584b702e27c7782b88956cc00e6261d1
BrayanGit <brayan.dario@outlook.com>
2023-11-03 11:28:37 -0400

    otros

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://brayangit.github.io/BrayanBG",
+  base: "https://brayangit.github.io/BrayanBG/",
 })
